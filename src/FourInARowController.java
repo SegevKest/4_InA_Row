aa2accdff5790de6988572c59256ff55aaeb0f41
Segev <segev.kest@gmail.com>
2022-04-30 00:15:57 +0300

    Fixed the Board - inserted accordingly. Started with logic part in the program

--- src/FourInARowController.java
+++ src/FourInARowController.java
@@ -1,14 +1,12 @@
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;import sun.java2d.DefaultDisposerRecord;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Border;
-import javafx.scene.layout.BorderStroke;
-import javafx.scene.layout.BorderStrokeStyle;
-import javafx.scene.layout.BorderWidths;
-import javafx.scene.layout.CornerRadii;
+
 
 
 public class FourInARowController {
@@ -28,12 +26,38 @@ public class FourInARowController {
     private Button[] btnsArrayGrid;
     private Pane[][] board;
     
+    private int selectedColumToInsert;
+    private boolean colorOfCircle;		// If true - will be red, false - yellow
     
     public void initialize() {
 	
-    	btnsArrayGrid = new Button[COLUMNS];
-    	board = new Pane[ROWS][COLUMNS];
+    	colorOfCircle = true;
     	
+    	initButtons();
+    	
+    	initGrid();
+    	
+    }
+
+    
+    @FXML
+    void clearGame(ActionEvent event) {
+
+    	
+    	for (int i=0; i<ROWS; i++) {
+    		for (int j =0; j<COLUMNS; j++) {
+    			board[i][j] = null;
+    			
+
+    		}
+    	}
+    	
+    }
+    
+    
+    private void initButtons() {
+    	
+    	btnsArrayGrid = new Button[COLUMNS];
     	
     	// Add the first row of the buttons + their text
     	for (int i = 0; i< COLUMNS; i++) {
@@ -41,7 +65,23 @@ public class FourInARowController {
     		btnsArrayGrid[i] = new Button(""+(i+1));	
     		btnsArrayGrid[i].setPrefSize(btnsGrid.getPrefWidth() / COLUMNS, btnsGrid.getPrefHeight());
     		btnsGrid.add( btnsArrayGrid[i], i, 0);
+    		
+    		btnsArrayGrid[i].setOnAction(new EventHandler<ActionEvent>() {
+				
+				@Override
+				public void handle(ActionEvent event) {
+					handleInsertButtonEvent(event);
+					
+				}
+
+			});
     	}
+    }
+    
+    
+    private void initGrid() {
+    	
+    	board = new Pane[ROWS][COLUMNS];
     	
     	for (int i=0;i<ROWS; i++) {
     		for( int j=0;j<COLUMNS; j++) {
@@ -54,19 +94,60 @@ public class FourInARowController {
     			gameBoard.add(board[i][j], j, i);
     		}
     	}
-    	
-    	
-    	
-    	
-    	
-    	
-    	
     }
-
     
-    @FXML
-    void clearGame(ActionEvent event) {
 
-    }
+	private void handleInsertButtonEvent(ActionEvent event) {
+		
+		Button clickedBtn = (Button)event.getSource();
+		System.out.println();
+		
+		selectedColumToInsert = Integer.parseInt(clickedBtn.getText());
+
+		insertNewDisk(selectedColumToInsert - 1);
+		
+		// call to the logic method to enter the new disk in the logic class
+		// return the status that will continue
+		// 4 - WIN , 2-3 continue, 1 found single, 0 - ended  
+		
+		//Send POPUP message on WINNER.
+		
+	}
+	
+	
+	private int getNextRowToInsertDisk(int currColumn) {
+		
+		int i=ROWS-1;
+		
+		while( board[ i][currColumn].getChildren().size() != 0) {
+			i--;
+		}
+
+		return i;
+	}
+	
+	
+	private void insertNewDisk(int currColumn) {
+		
+		//double newDiskRadius = ( gameBoard.getPrefWidth() / COLUMNS);
+		double newDiskRadius = (20);
+
+		Circle newDisk = new Circle(29, 25, newDiskRadius);
+		
+		System.out.println(newDisk.getCenterX()+" ; "+newDisk.getCenterY());
+		int rowToInsert = getNextRowToInsertDisk(currColumn);
+		
+		if( colorOfCircle ) 
+			newDisk.setFill(Color.RED);
+		else
+			newDisk.setFill(Color.YELLOW);
+		
+		board[ rowToInsert][currColumn].getChildren().add(newDisk);
+		
+		System.out.println(board[ rowToInsert][currColumn].getPrefHeight()+" - " +board[ rowToInsert][currColumn].getPrefWidth());
+		
+		colorOfCircle = !colorOfCircle;
+		
+	}
 
 }
